8d7d1f575e4ab965f42cf899bf025b8cdbbf229f
Vladimir <malleerx@gmail.com>
2022-01-13 15:35:24 +0300

    Init commit

---
+++ src/main/java/com/malerx/WeatherHistory/WeatherHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.malerx.WeatherHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherHistoryApplication.class, args);
+	}
+
+}
